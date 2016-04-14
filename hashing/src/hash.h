060d244bdb9647d901014afa318cf1cb7ebe8f39
Jason C. Rochon <jcrochon@cpan.org>
2016-04-14 08:55:47 -0500

    First commit

---
+++ hashing/src/hash.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Apr 14, 2016
+ *      Author: jcrochon
+ */
+
+#ifndef HASH_H_
+#define HASH_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void title(void) {
+	printf("\nI'm a Hash!\n");
+}
+#endif /* HASH_H_ */
---
+++ hashing/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 14, 2016
+ *      Author: jcrochon
+ */
+
+#include "hash.h"
+
+int main(void) {
+	title();
+	return(0);
+}
